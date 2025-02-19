0278ade1942bc20ae08deb8bbd89c9a0dd83c0a4
Yxoro <yann.poupeau1@gmail.com>
2025-02-19 18:23:48 +0100

    Create filter.py

---
+++ PyxelPalette/filter.py
@@ -0,0 +1,29 @@
+from PIL import ImageFilter
+
+def apply_blur(image, radius=2):
+    return image.filter(ImageFilter.GaussianBlur(radius))
+
+def apply_sepia(image):
+    width, height = image.size
+    pixels = image.load()
+
+    for py in range(height):
+        for px in range(width):
+            r, g, b = image.getpixel((px, py))
+
+            tr = int(0.393 * r + 0.769 * g + 0.189 * b)
+            tg = int(0.349 * r + 0.686 * g + 0.168 * b)
+            tb = int(0.272 * r + 0.534 * g + 0.131 * b)
+
+            if tr > 255:
+                tr = 255
+
+            if tg > 255:
+                tg = 255
+
+            if tb > 255:
+                tb = 255
+
+            pixels[px, py] = (tr, tg, tb)
+
+    return image
